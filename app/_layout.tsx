84c8bf9fdf8ecc5f83bea02500c676eb9c336214
administrador <pedrorodacinski26@gmail.com>
2025-06-09 08:12:37 -0300

    Update nas telas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
-import { Stack } from 'expo-router';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ClientesProvider } from '../src/screens/functions/ClientesContext';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Login from './index';
+import HomeScreen from './home/HomeScreen'; // ajuste se necessário
+
+const Stack = createNativeStackNavigator();
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <SafeAreaProvider>
+      <ClientesProvider>
+          <Stack.Navigator initialRouteName="Login" screenOptions={{ title: 'Home', headerShown: false }}>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Home" component={HomeScreen} />
+            {/* Adicione outras telas aqui */}
+          </Stack.Navigator>
+      </ClientesProvider>
+    </SafeAreaProvider>
+  );
 }
